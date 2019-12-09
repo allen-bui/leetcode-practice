35c6760fe754792bc70750eadeb921dff911b47c
allen-bui <allen.vbui@gmail.com>
2019-12-09 15:37:52 -0800

    completes missing numbers

--- missingNumbers.js
+++ missingNumbers.js
@@ -4,4 +4,17 @@
  */
 var findDisappearedNumbers = function(nums) {
 
+  const result = [];
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] = 1;
+  }
+
+  for (let j = 1; j < nums.length + 1; j++) {
+    if (!(String(j) in hash)) {
+      result.push(j);
+    }
+  }
+  return result;
 };
