91679061a2956fe001602ff63c3007795b6997da
allen-bui <allen.vbui@gmail.com>
2019-12-16 14:45:49 -0800

    improves space complexity from O(N) to O(1)

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -8,23 +8,18 @@
 var productExceptSelf = function(nums) {
 
   // declare variables
-  const left = [1];
-  const right = [1];
-  const result = [];
+  const result = new Array(nums.length).fill(1);
+  let multiplier = 1;
 
   // loop through nums array, multiply everything on the left side together
-  for (let i = 0; i < nums.length - 1; i++) {
-    left.push(nums[i] * left[i]);
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = nums[i - 1] * result[i - 1];
   }
 
   // loop through nums array, multiply everything on the right side together
   for (let j = nums.length - 1; j > 0; j--) {
-    right.unshift(nums[j] * right[0]);
-  }
-
-  // loop through nums array, multiplying left and right index together
-  for (let k = 0; k < nums.length; k++) {
-    result[k] = left[k] * right[k];
+    result[j] = result[j] * multiplier;
+    multiplier = nums[j] * multiplier;
   }
   return result;
 };
