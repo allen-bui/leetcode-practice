51b0cead9acfdab8dbf1cf300053cf3caef5f33d
allen-bui <allen.vbui@gmail.com>
2019-12-16 18:54:34 -0800

    adds time complexity and commentary

--- rotateArray.js
+++ rotateArray.js
@@ -1,3 +1,6 @@
+// Time Complexity - O(1)
+// Space Complexity - O(2 x N)
+
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -5,10 +8,13 @@
  */
 var rotate = function(nums, k) {
 
+  // mod K in case shift is longer than length of nums
   const modK = k % (nums.length / 2);
 
+  // double the length of nums, since solution will always fall into this range
   nums = nums.concat(nums);
 
+  // slice array from shifting point to end of shifting point
   const res = nums.slice(nums.length / 2 - modK, nums.length - modK);
 
   return res;
