e573a7c3009e6585ef08e5743b0da8d4a9865bad
allen-bui <allen.vbui@gmail.com>
2019-12-16 18:19:04 -0800

    adds commentary and time complexity

--- happyNumber.js
+++ happyNumber.js
@@ -1,19 +1,29 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
 /**
  * @param {number} n
  * @return {boolean}
  */
 var isHappy = function(n, key) {
+  // memoize the numbers we've seen before
+  key ? key : (key = {});
 
-  key ? key : key = {};
+  // calculate square of the number
+  const sumSquare = String(n)
+    .split('')
+    .map((number) => number ** 2)
+    .reduce((a, b) => a + b);
 
-  const sumSquare = String(n).split('').map(number => number ** 2).reduce((a, b) => a + b);
-
+  // if we've seen the number, return false since it will be an infinite loop
   if (key[sumSquare]) {
     return false;
   }
+  // if the number squares to one, then return true
   else if (sumSquare === 1) {
     return true;
   }
+  // keep recursing until we hit 1 or infinite loop which will be caught by the key
   else {
     key[sumSquare] = true;
     return isHappy(sumSquare, key);
