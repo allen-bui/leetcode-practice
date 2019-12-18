572276c11a1ee7c6795f69249df02e81dfe8a222
allen-bui <allen.vbui@gmail.com>
2019-12-17 16:19:24 -0800

    adds working pop method to linked list class

--- linkedList.js
+++ linkedList.js
@@ -14,19 +14,58 @@ class SinglyLinkedList {
 
   push(value) {
 
+    // declare new variable
     const node = new Node(value);
 
+    // if head is null, then set head and tail to same node
     if (this.head === null) {
       this.head = node;
       this.tail = node;
     }
+    // otherwise assign the next tail value to new node and update
+    // tail to new node
     else {
       this.tail.next = node;
       this.tail = node;
     }
+    // add to length to keep track
     this.length += 1;
     return this;
   }
+
+  pop() {
+
+    if (this.head === null) {
+      return undefined;
+    }
+
+    // declare variables
+    let current = this.head;
+    let newTail = current;
+
+    // keep looping until we hit the end of our linked list
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    // assign new tail and set the next value to null
+    this.tail = newTail;
+    this.tail.next = null;
+
+    // adjust length
+    this.length -= 1;
+
+    // reset head and tail to null if length is 0
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current.value;
+  }
+
+
+
 }
 
 const ll = new SinglyLinkedList();
@@ -34,5 +73,4 @@ const ll = new SinglyLinkedList();
 ll.push(5);
 ll.push(2);
 ll.push(3);
-
-console.log(ll);
+console.log(ll.pop()); // 3
