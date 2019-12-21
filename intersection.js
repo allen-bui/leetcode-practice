77f2b20ef870c00be5a960282a9d62b47c586bc5
allen-bui <allen.vbui@gmail.com>
2019-12-20 18:35:21 -0800

    adds time and space complexity, and comments

--- intersection.js
+++ intersection.js
@@ -1,3 +1,5 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
 
 /**
  * @param {number[]} nums1
@@ -6,9 +8,11 @@
  */
 var intersection = function(nums1, nums2) {
 
+  // create set out of nums 1 and result
   const setOne = new Set(nums1);
   const res = new Set();
 
+  // if setOne has a value in nums2, add to result
   for (let i = 0; i < nums2.length; ++i) {
     if (setOne.has(nums2[i])) {
       res.add(nums2[i]);
