e7f65d0d3d3671ad78e6c16803864597538e67bd
allen-bui <allen.vbui@gmail.com>
2019-12-21 16:35:48 -0800

    working pre order traversal method

--- treeStructure.js
+++ treeStructure.js
@@ -15,17 +15,30 @@ class Tree {
     const child = new Node(value);
     this.children.push(child);
   }
+
+  preOrderTraversal(node = this, arr = []) {
+
+    arr.push(node.value);
+
+    for (let i = 0; i < node.children.length; ++i) {
+      const child = node.children[i];
+      this.preOrderTraversal(child, arr);
+    }
+    return arr;
+  }
 }
 
 const tree = new Tree(1);
 tree.addChild(2);
 tree.addChild(3);
 tree.addChild(4);
-tree.children[0].children = new Node(5);
-tree.children[0].children = new Node(6);
-tree.children[1].children = new Node(7);
-tree.children[1].children = new Node(8);
-tree.children[2].children = new Node(9);
-tree.children[2].children = new Node(10);
+tree.children[0].children.push(new Node(5));
+tree.children[0].children.push(new Node(6));
+tree.children[1].children.push(new Node(7));
+tree.children[1].children.push(new Node(8));
+tree.children[2].children.push(new Node(9));
+tree.children[2].children.push(new Node(10));
 
-console.log(tree);
+// console.log(tree.children[0]);
+
+console.log(tree.inOrderTraversal()); // [1, 2, 5, 6, 3, 7, 8, 4, 9, 10];
