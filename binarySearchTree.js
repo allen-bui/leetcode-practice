5834b5dde88419ee17af484c97615c69a8e3f0d1
allen-bui <allen.vbui@gmail.com>
2019-12-22 17:33:32 -0800

    adds 'search' method to binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -15,13 +15,24 @@ class BinarySearchTree {
 
   add(value, root = this) {
     const node = new Node(value);
-
     if (node.value < root.value) {
       root.left === null ? (root.left = node) : this.add(value, root.left);
     } else if (node.value > root.value) {
       root.right === null ? (root.right = node) : this.add(value, root.right);
     } else {
-      console.log(`[Error]: The value "${value}" is already inside binary search tree.`);
+      console.log(
+        `[Error]: The value "${value}" is already inside binary search tree.`
+      );
+    }
+  }
+
+  search(value, root = this) {
+    if (value === root.value) {
+      return true;
+    } else if (value < root.value) {
+      return root.left === null ? false : this.search(value, root.left);
+    } else {
+      return root.right === null ? false : this.search(value, root.right);
     }
   }
 }
@@ -35,3 +46,5 @@ tree.add(6);
 tree.add(8);
 
 console.log(tree);
+console.log(tree.search(8)); // true
+console.log(tree.search(10)); // false
