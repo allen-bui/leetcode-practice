64d2e7a8254c398310ecabacc228169e19b5344d
allen-bui <allen.vbui@gmail.com>
2019-12-22 17:24:31 -0800

    adds working add method to binary search tree

---
+++ binarySearchTree.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(value, root = this) {
+    const node = new Node(value);
+
+    if (node.value < root.value) {
+      root.left === null ? (root.left = node) : this.add(value, root.left);
+    } else if (node.value > root.value) {
+      root.right === null ? (root.right = node) : this.add(value, root.right);
+    } else {
+      return '[Error]: Value is already inside binary search tree.'
+    }
+  }
+}
+
+const tree = new BinarySearchTree(5);
+tree.add(3);
+tree.add(2);
+tree.add(4);
+tree.add(7);
+tree.add(6);
+tree.add(8);
+
+console.log(tree);
