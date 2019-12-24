4ae5f7d947830c2124880dc915d7723155bece85
allen-bui <allen.vbui@gmail.com>
2019-12-23 22:37:44 -0800

    adds commentary and time/space complexity

--- increaseDecreaseString.js
+++ increaseDecreaseString.js
@@ -1,11 +1,15 @@
+// Time Complexity  - O(N)
+// Space Complexity - O(N)
+
 var diStringMatch = function(S) {
 
+  // declare variables
   let increase = 0;
   let decrease = S.length;
   const result = [];
 
+  // loop through string S, pushing a decrease or increase value based on string character
   for (let i = 0; i < S.length; ++i) {
-
     if (S[i] === 'I') {
       result.push(increase++);
     }
