5515b43ec4b7c6021396566ed63783abbd0b0c97
allen-bui <allen.vbui@gmail.com>
2019-12-26 22:27:20 -0800

    adds time and space complexity

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,14 +1,6 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+// Time Complexity -  O(N)
+// Space Complexity - O(N)
+
 var reverseList = function(head) {
 
   let prev = null;
