c2e36b275f0af3bfa606085640e01c1fb541463c
allen-bui <allen.vbui@gmail.com>
2019-12-27 17:26:13 -0800

    adds notes, commentary, and time/space complexity

--- dpBottomUpFibonacci.js
+++ dpBottomUpFibonacci.js
@@ -1,10 +1,18 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
+// NOTE
+// We could make this O(1) space complexity by only storing the N-1 and N-2 value
+// deleting the older values as we continue our loop
+
 function dpBottomUpFibonacci(n) {
 
+  // create hash
   const fib = {};
   let sum;
 
-  for (let i = 1; i < n+1; ++i) {
-
+  // loop through each number until n, and store sum of previous two numbers
+  for (let i = 1; i < n + 1; ++i) {
     if (i <= 2) {
       sum = 1;
     } else {
