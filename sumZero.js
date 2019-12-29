4e8b9d60120b2b17d68c791c3fde2b1368568f54
allen-bui <allen.vbui@gmail.com>
2019-12-28 22:13:43 -0800

    completes sum zero

---
+++ sumZero.js
@@ -0,0 +1,17 @@
+var sumZero = function(n) {
+
+  const result = [];
+
+  if (n % 2 === 0) {
+      for (let i = (n/2) * -1; i <= n/2; ++i) {
+          if (i === 0) continue;
+          else result.push(i);
+      }
+  }
+  else {
+      for (let i = Math.floor(n/2) * -1; i < Math.ceil(n/2); ++i) {
+          result.push(i);
+      }
+  }
+  return result;
+};
