e670a4a167afcbaa29b0e27703c30ad7e4cf7bd2
allen-bui <allen.vbui@gmail.com>
2019-12-30 15:58:05 -0800

    completes coin flip

---
+++ coinFlip.js
@@ -0,0 +1,16 @@
+function coinFlip(n) {
+  const data = {
+    heads: 0,
+    tails: 0,
+  };
+
+  for (let i = 0; i < n; ++i) {
+    const flip = Math.floor(Math.random() * 100) + 1; // changes range from 0-99 to 1-100
+
+    if (flip <= 50) data.heads += 1;
+    else data.tails += 1;
+  }
+  return data;
+}
+
+console.log(coinFlip(1000000));
