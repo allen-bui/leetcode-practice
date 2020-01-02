a1e87cea46288c0e1489c06a6fa39f1e07966be3
allen-bui <allen.vbui@gmail.com>
2020-01-01 21:10:13 -0800

    shortens code and turns space complexity from O(N) to O(1) by manipulating original array

--- kClosestPoints.js
+++ kClosestPoints.js
@@ -1,26 +1,11 @@
 // Time Complexity -  O(N log N)
-// Space Complexity - O(N)
+// Space Complexity - O(1)
 
 var kClosest = function(points, K) {
-
-  // declare hash table
-  const result = [];
-  const map = {};
-
-  // loop through array, calculate distance and store as index
-  for (let i = 0; i < points.length; ++i) {
-    map[i] = calculateDistance(points[i]);
-  }
-
-  // sort the distances from least to greatest
-  const temp = Object.entries(map).sort((a, b) => a[1] - b[1]);
-
-  // loop through sorted distance and go up to K, pushing into result array
-  for (let j = 0; j < K; ++j) {
-    const shortestPoint = Number(temp[j][0]);
-    result.push(points[shortestPoint]);
-  }
-  return result;
+  // sort by shortest distance, and return sliced array from 0 to K
+  return points
+    .sort((a, b) => calculateDistance(a) - calculateDistance(b))
+    .slice(0, K);
 };
 
 function calculateDistance(coordinate) {
@@ -28,3 +13,5 @@ function calculateDistance(coordinate) {
   const y = coordinate[1] ** 2;
   return Math.sqrt(x + y);
 }
+
+console.log(kClosest([[1,3],[-2,2]], 1)); // [[-2, 2]]
