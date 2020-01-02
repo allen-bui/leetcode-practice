44deed167eeb18eb96a9269d7f33e2694df59cbd
allen-bui <allen.vbui@gmail.com>
2020-01-01 21:01:14 -0800

    completes k closest points

--- kClosestPoints.js
+++ kClosestPoints.js
@@ -1,17 +1,25 @@
 var kClosest = function(points, K) {
 
+  const result = [];
+  const map = {};
 
+  for (let i = 0; i < points.length; ++i) {
+    map[i] = calculateDistance(points[i]);
+  }
 
+  const temp = Object.entries(map).sort((a, b) => a[1] - b[1]);
 
+  for (let j = 0; j < K; ++j) {
+    const shortestPoint = Number(temp[j][0]);
+    result.push(points[shortestPoint]);
+  }
 
+  return result;
 };
 
 function calculateDistance(coordinate) {
-
   const x = coordinate[0] ** 2;
   const y = coordinate[1] ** 2;
 
   return Math.sqrt(x + y);
 }
-
-console.log(calculateDistance([-2, 2]));
