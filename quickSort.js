75d6d349a6d1b1bb7133ce4811b24b93c1839124
allen-bui <allen.vbui@gmail.com>
2020-01-02 18:37:27 -0800

    working quick sort algorithm with swap and partition methods

--- quickSort.js
+++ quickSort.js
@@ -4,24 +4,32 @@ function swap(arr, leftIndex, rightIndex) {
   arr[rightIndex] = temp;
 }
 
-function partition(arr, left, right) {
+function partition(arr, left, right, pivot) {
 
-  const pivot = arr[Math.floor((left + right) / 2)];
-  const i = left;
-  const j = right;
-
-  while (i <= j) {
-    while(arr[i] < pivot) {
-      i++;
+  while (left <= right) {
+    while (arr[left] < arr[pivot]) {
+      left++;
     }
-    while (arr[j] > pivot) {
-      j--;
+    while (arr[right] > arr[pivot]) {
+      right--;
     }
-    if (i <= j) {
-      swap(arr, i, j);
-      i++;
-      j--;
+    if (left <= right) {
+      swap(arr, left, right);
+      left++;
+      right--;
     }
   }
-  return i;
+  return left;
 }
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left >= right) return;
+  const pivot = Math.floor((left + right) / 2);
+  const index = partition(arr, left, right, pivot);
+  quickSort(arr, left, index - 1);
+  quickSort(arr, index, right);
+  return arr;
+}
+
+const unsortedArray = [5, 3, 7, 6, 2, 9];
+console.log(quickSort(unsortedArray, 0, unsortedArray.length - 1));
