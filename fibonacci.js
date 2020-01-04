d7deba52a40fdb3c7bc2b83f2964ccacd733880e
allen-bui <allen.vbui@gmail.com>
2020-01-03 19:02:12 -0800

    solves fibonacci with dynamic programming for faster run time

--- fibonacci.js
+++ fibonacci.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var fib = function(N) {
-  if (N === 0) {
-    return 0;
+  if (N === 0) return 0;
+  const arr = [];
+  arr[0] = 1;
+  arr[1] = 1;
+
+  for (let i = 2; i < N; ++i) {
+    arr[i] = arr[i - 1] + arr[i - 2];
   }
-  else if (N <= 2) {
-    return 1;
-  }
-  return fib(N - 1) + fib(N - 2);
+  return arr[N - 1];
 };
