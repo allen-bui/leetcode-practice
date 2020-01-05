0789ad769ccdbcf1c4b37d6d3f1f4d27bebe8d33
allen-bui <allen.vbui@gmail.com>
2020-01-04 20:25:14 -0800

    adds commentary

--- dailyTemperature.js
+++ dailyTemperature.js
@@ -2,11 +2,15 @@
 // Space Complexity - O(N)
 
 var dailyTemperatures = function(T) {
+
   let result = new Array(T.length).fill(0);
   let stack = [];
 
+  // the idea is to use a stack to keep track of the index
+  // when the current iteration emperature is higher than the
+  // latest stack index, then replace the result index
+  // with current iteration minus index
   for (let i = 0; i < T.length; ++i) {
-
     while(stack.length > 0 && T[i] > T[stack[stack.length - 1]]) {
       let index = stack.pop();
       result[index] = i - index;
