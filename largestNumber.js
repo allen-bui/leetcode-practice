4fe7453b9d40918d3ca1c1fa420a46209168ed77
allen-bui <allen.vbui@gmail.com>
2020-01-04 21:43:48 -0800

    initial commit with pseudocode

---
+++ largestNumber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+
+  // use a hash map with keys 9 through 0 (9,8,7...0)
+  // first pass: loop through each number in array and put in correct hash
+  // second pass: order the numbers in the array by length, if same length
+  //              then order descending
+  // third pass: loop through hash and concatenate string together
+
+};
