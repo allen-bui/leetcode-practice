6381bed6345ef4d1a64a7e42e8eab581d5df8078
allen-bui <allen.vbui@gmail.com>
2020-01-05 15:58:19 -0800

    adds commentary, time and space complexity

--- largestNumber.js
+++ largestNumber.js
@@ -1,4 +1,10 @@
+// Time Complexity -  O(N log N)
+// Space Complexity - O(1)
+
 var largestNumber = function(nums) {
+  // sort the array by concatenating numbers together and seeing
+  // which makes the larger number, then join string together and
+  // remove unnecessary zeroes if any
   return nums
     .sort((a, b) => `${b}${a}` - `${a}${b}`)
     .join('')
