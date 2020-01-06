23467b6c6b70f5cc7ebc6bb4ed3836f1f6a45fc2
allen-bui <allen.vbui@gmail.com>
2020-01-05 21:39:47 -0800

    adds problem link and commentary to assist anyone looking at the code

--- shortestDistanceToCharacter.js
+++ shortestDistanceToCharacter.js
@@ -1,3 +1,5 @@
+// LeetCode Problem #821 - https://leetcode.com/problems/shortest-distance-to-a-character/
+
 /**
  * @Time  - O(N)
  * @Space - O(N)
@@ -7,12 +9,15 @@ var shortestToChar = function(S, C) {
   const characterIndices = [];
   const result = [];
 
+  // loop through to find indices of C
   for (let i = 0; i < S.length; ++i) {
     if (S[i] === C) {
       characterIndices.push(i);
     }
   }
 
+  // loop through S, calculate the distance between indicies while i is
+  // not equal to next, if equal, then move next to the next C index
   for (let i = 0, j = 0; i < S.length; ++i) {
     const next = Math.min(j + 1, characterIndices.length - 1);
     result.push(Math.min(Math.abs(i - characterIndices[j]), Math.abs(i - characterIndices[next])));
