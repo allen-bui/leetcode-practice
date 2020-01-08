6d52fa275244740916db879e71e0e2d7696151f8
allen-bui <allen.vbui@gmail.com>
2020-01-08 15:40:19 -0800

    cleans up code and adds commentary

--- leafSimilarTrees.js
+++ leafSimilarTrees.js
@@ -6,6 +6,7 @@
  */
 var leafSimilar = function(root1, root2) {
 
+  // get leaf nodes for each tree
   const treeOneLeaves = findLeaves(root1);
   const treeTwoLeaves = findLeaves(root2);
 
@@ -13,6 +14,8 @@ var leafSimilar = function(root1, root2) {
     return false;
   }
 
+  // loop through leaf nodes and ensure the sequence is the same
+  // otherwise return false
   for (let i = 0; i < treeOneLeaves.length; ++i) {
     if (treeOneLeaves[i] !== treeTwoLeaves[i]) {
       return false;
@@ -21,20 +24,11 @@ var leafSimilar = function(root1, root2) {
   return true;
 };
 
+// recursive helper function to find all the leaf nodes
+// returns an array of leaf nodes
 function findLeaves(root, result = []) {
-
-  if (root.left === null && root.right === null) {
-    result.push(root.val);
-  };
-
-  if (root.left !== null) {
-    findLeaves(root.left, result);
-  }
-
-  if (root.right !== null) {
-    findLeaves(root.right, result);
-  }
+  if (root.left === null && root.right === null) result.push(root.val);
+  if (root.left !== null) findLeaves(root.left, result);
+  if (root.right !== null) findLeaves(root.right, result);
   return result;
 }
-
-console.log(findLeaves(node));
