601872150cc9b382fc0c43a13b795356b1cfa0f7
allen-bui <allen.vbui@gmail.com>
2020-01-08 14:07:21 -0800

    adds time complexity, space complexity, and commentary

--- findComplementNumber.js
+++ findComplementNumber.js
@@ -1,16 +1,22 @@
+// LeetCode Problem #476 - https://leetcode.com/problems/number-complement/
+
 /**
- * @param {number} num
- * @return {number}
+ * @Time  - O(N)
+ * @Space - O(1)
  */
 var findComplement = function(num) {
 
+  // convery number to binary
   const binaryNum = num.toString(2);
   let result = '';
 
+  // loop through each binary number and add the inverse to result string
   for (const binaryDigit of binaryNum) {
     if (binaryDigit === '0') result += '1';
     else result += '0';
   }
+
+  // return base 10 number of result
   return parseInt(result, 2);
 };
 
