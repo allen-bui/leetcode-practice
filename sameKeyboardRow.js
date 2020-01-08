153bdb0079d9f45b63f75b009c0c61ccc041f8ae
allen-bui <allen.vbui@gmail.com>
2020-01-07 22:13:28 -0800

    adds commentary

---
+++ sameKeyboardRow.js
@@ -0,0 +1,11 @@
+// LeetCode Problem #500 - https://leetcode.com/problems/keyboard-row/
+
+/**
+ * @Time  -
+ * @Space -
+ */
+var findWords = function(words) {
+    // use a map to figure out which letters are on the same row
+    // loop through letter and make sure it has the same constant
+};
+
