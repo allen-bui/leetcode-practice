000b1202a73b9755d9c8e3525c54d0eab5e8c420
allen-bui <allen.vbui@gmail.com>
2020-01-08 21:37:07 -0800

    makes code more readable

--- validateBinarySearchTree.js
+++ validateBinarySearchTree.js
@@ -10,14 +10,10 @@ var isValidBST = function(root) {
   }
   return dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
 
+  // helper function that traverses tree
   function dfs(root, min, max) {
-    if (!root){
-      return true;
-    }
-    if (root.val <= min || root.val >= max) {
-      return false;
-    }
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
     return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
   }
 };
-
