85f83542af674d0b78b38dad7c256f306cd308a2
allen-bui <allen.vbui@gmail.com>
2020-01-09 19:40:39 -0800

    completes maximum balloons

---
+++ maximumBalloons.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+  const balloonMap = {
+    b: 1,
+    a: 1,
+    l: 2,
+    o: 2,
+    n: 1
+  };
+  const letterMap = {};
+
+  for (const letter of text) {
+    letterMap[letter] = letterMap[letter] ? (letterMap[letter] += 1) : 1;
+  }
+
+  for (const letter in balloonMap) {
+    if (!(letter in letterMap) || balloonMap[letter] > letterMap[letter]) {
+      return 0;
+    }
+  }
+  return Math.min(Math.floor(balloonMap.l / 2), Math.floor(balloonMap.o / 2));
+};
