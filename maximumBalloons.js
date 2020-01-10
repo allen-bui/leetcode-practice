ec35be99aa5c5bae11df48464bde38aed0072330
allen-bui <allen.vbui@gmail.com>
2020-01-09 20:12:14 -0800

    adds time and space complexity, and commentary

--- maximumBalloons.js
+++ maximumBalloons.js
@@ -1,8 +1,13 @@
+// LeetCode Problem #1189 - https://leetcode.com/problems/maximum-number-of-balloons/
+
 /**
- * @param {string} text
- * @return {number}
+ * @Time  - O(N)
+ * @Space - O(1)
  */
 var maxNumberOfBalloons = function(text) {
+
+  // store letter occurances inside the word "balloon"
+  // store letter inside
   const balloonMap = {
     b: 1,
     a: 1,
@@ -12,14 +17,20 @@ var maxNumberOfBalloons = function(text) {
   };
   const letterMap = {};
 
+  // first pass: loop through text and add into hash map
   for (const letter of text) {
     letterMap[letter] = letterMap[letter] ? (letterMap[letter] += 1) : 1;
   }
 
+  // second pass: compare the balloon map and text map, if any letter in balloon
+  // is not in text map, or balloon map has more characters than text map, return zero
   for (const letter in balloonMap) {
     if (!(letter in letterMap) || balloonMap[letter] > letterMap[letter]) {
       return 0;
     }
   }
+
+  // now that we know the text map has at least one 'balloon' combination,
+  // take the minimium of the 'l' or 'o' letter divided by two since there's two occurances
   return Math.min(Math.floor(letterMap.l / 2), Math.floor(letterMap.o / 2));
 };
