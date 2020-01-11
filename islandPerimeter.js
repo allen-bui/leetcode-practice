2ac2ca73f79a76f637836c804c9db855f733cfc3
allen-bui <allen.vbui@gmail.com>
2020-01-10 20:32:11 -0800

    completes island perimeter

---
+++ islandPerimeter.js
@@ -0,0 +1,19 @@
+function islandPerimeter(grid) {
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let perimeter = 0;
+
+  for (let row = 0; row < rows; ++row) {
+    for (let col = 0; col < cols; ++col) {
+      if (!grid[row][col]) continue;
+      perimeter += 4;
+
+      if (row > 0 && grid[row - 1][col]) perimeter--;
+      if (col > 0 && grid[row][col - 1]) perimeter--;
+      if (row < rows - 1 && grid[row + 1][col]) perimeter--;
+      if (col < cols - 1 && grid[row][col + 1]) perimeter--;
+    }
+  }
+  return perimeter;
+}
