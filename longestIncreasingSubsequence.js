83b1583616d505a94e76b8fcc558b5cb505927f9
allen-bui <allen.vbui@gmail.com>
2020-01-11 16:31:53 -0800

    working function except for full array increase bug

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -5,11 +5,26 @@
 var findLengthOfLCIS = function(nums) {
 
   // use two pointers (still and mover), start at 0
-  // increment mover until current index is lower than previous index
-  // if decrease
-  //    move still to mover index
-  //    take max of current and max
-  //    reset counter index
-  // return max
+  let current = 1;
+  let max = 1;
+  let still = 0;
+  let mover = 1;
 
+  // increment mover until current index is lower than previous index
+  while (mover !== nums.length - 1) {
+
+    if (nums[mover] <= nums[mover - 1]) {
+      max = Math.max(current, max);
+      current = 1;
+      still = mover;
+      mover += 1;
+    }
+    else {
+      current += 1;
+      mover += 1;
+    }
+  }
+  return max;
 };
+
+console.log(findLengthOfLCIS([1, 3, 5, 6, 7]));
