6f4ff120273ec1e7ad8c034a701183506f048149
allen-bui <allen.vbui@gmail.com>
2020-01-11 16:21:34 -0800

    completes pseudocode for problem

---
+++ longestIncreasingSubsequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+
+  // use two pointers (still and mover), start at 0
+  // increment mover until current index is lower than previous index
+  // if decrease
+  //    move still to mover index
+  //    take max of current and max
+  //    reset counter index
+  // return max
+
+};
