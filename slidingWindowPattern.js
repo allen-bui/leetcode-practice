6d5588474f6466c998073d657f46cac2509acd11
allen-bui <allen.vbui@gmail.com>
2020-01-11 16:00:27 -0800

    adds commentary, time and space complexity

--- slidingWindowPattern.js
+++ slidingWindowPattern.js
@@ -1,12 +1,20 @@
+// Time Complexity  - O(N)
+// Space Complexity - O(1)
+
 function maximumSubArray(arr, n) {
 
   let currentSum = 0;
-  let maxSum = Number.MIN_SAFE_INTEGER;
+  let maxSum;
 
+  // loop through array up to nth-digit and add up sum
+  // set max sum equal to the current sum after loop;
   for (let i = 0; i < n; ++i) {
     currentSum += arr[i];
   }
   maxSum = currentSum;
+
+  // subtract the previous element, then add the next element
+  // set max sum to whichever is higher, currentSum and maxSum
   for (let j = n - 1; j < arr.length - 1; ++j) {
     currentSum = currentSum - arr[j - n + 1] + arr[j + 1];
     maxSum = Math.max(currentSum, maxSum);
