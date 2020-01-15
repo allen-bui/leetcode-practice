637ad4d095cd4a025f2a2a286ca9116654d51b96
allen-bui <allen.vbui@gmail.com>
2020-01-15 15:43:18 -0800

    adds commentary/pseudocode

--- rangeSumBST.js
+++ rangeSumBST.js
@@ -3,6 +3,15 @@
 
 var rangeSumBST = function(root, L, R) {
 
+  /*
+  Pseudocode
+  ----------
+  1. initiate a sum variable
+  2. traverse tree and make sure each node visited is greater/equal than left or less/equal than right
+  3. if true, then add to sum variable
+  3. return sum
+  */
+
   let sum = 0;
   traverse(root);
   return sum;
