b030bc3ebce7902d4114c75cd98aefcf82255f9f
allen-bui <allen.vbui@gmail.com>
2020-01-15 15:41:46 -0800

    refactors code for readability

--- rangeSumBST.js
+++ rangeSumBST.js
@@ -1,28 +1,16 @@
 // Time Complexity  - O(log n)
 // Space Complexity - O(1)
 
-let sum;
-
 var rangeSumBST = function(root, L, R) {
-  sum = 0;
-  helper(root, L, R);
-  return sum;
-};
 
-function helper(root, L, R) {
-  // base case
-  if (!root) {
-    return 0;
+  let sum = 0;
+  traverse(root);
+  return sum;
+
+  // helper function to traverse tree and check node value
+  function traverse(root) {
+    if (root.val >= L && root.val <= R) sum += root.val;
+    if (root.left !== null) traverse(root.left);
+    if (root.right !== null) traverse(root.right);
   }
-  // add to sum if current node is between L and R
-  if (root.val >= L && root.val <= R) {
-    sum += root.val;
-  }
-  // recurse L and R nodes if value falls in range
-  if (root.val > L) {
-    helper(root.left, L, R);
-  }
-  if (root.val < R) {
-    helper(root.right, L, R);
-  }
-}
+};
