cf18c1f1cbf7b748493ae7e257e4bcfc2cb065c3
allen-bui <allen.vbui@gmail.com>
2020-01-20 18:52:39 -0800

    completes maximimum six-nine number

--- maximum69Number.js
+++ maximum69Number.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/maximum-69-number/
+
 /**
  * @param {number} num
  * @return {number}
@@ -6,4 +8,16 @@ var maximum69Number  = function(num) {
   // loop through nums
   // find the first six and replace it with a nine
   // return nums
+
+  let stringNum = String(num).split('');
+
+  for (let i = 0; i < stringNum.length; ++i) {
+    if (stringNum[i] === '6') {
+      stringNum[i] = '9';
+      break;
+    }
+  }
+  return Number(stringNum.join(''));
 };
+
+console.log(maximum69Number(9669));
