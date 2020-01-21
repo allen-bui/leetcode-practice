ccc445e302f2d71b38099ec55f0e6b6033b96429
allen-bui <allen.vbui@gmail.com>
2020-01-20 18:49:14 -0800

    initial commit with pseudocode on solving the problem

---
+++ maximum69Number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+  // loop through nums
+  // find the first six and replace it with a nine
+  // return nums
+};
