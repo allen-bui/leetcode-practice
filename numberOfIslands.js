22f5a29f0195c6107b7f605d5712a1259e568ee3
allen-bui <allen.vbui@gmail.com>
2020-01-24 16:07:43 -0800

    adds time and space complexity, and commentary

--- numberOfIslands.js
+++ numberOfIslands.js
@@ -1,15 +1,17 @@
 /**
- * @param {character[][]} grid
- * @return {number}
+ * @Time  - O(M * N) where M is rows and N is columns
+ * @Space - O(M * N)
  */
 var numIslands = function(grid) {
 
+  // Check for edge cases
   if (grid === null || grid.length === 0) {
     return 0;
   }
 
   let numberOfIslands = 0;
 
+  // Loop through each number and perform a DFS search
   for (let i = 0; i < grid.length; ++i) {
     for (let j = 0; j < grid[i].length; ++j) {
       if (grid[i][j] === '1') {
@@ -19,7 +21,10 @@ var numIslands = function(grid) {
   }
   return numberOfIslands;
 
-  // helper functions
+  // Helper Functions
+  //
+  // The trick with DFS is to 'sink' each island once you visted, then perform DFS for top, bottom, left, and right
+  // Once finished, to return 1 for the island since you're done traversing
   function dfs(grid, i, j) {
     if ( i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
       return 0;
