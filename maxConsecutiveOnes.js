14dac1bb27395dc430eff4d1b57a1a635603a396
allen-bui <allen.vbui@gmail.com>
2020-01-31 18:26:53 -0800

    completes max consecutive ones

---
+++ maxConsecutiveOnes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+
+  let maxSequence = 0;
+  let current = 0;
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] === 1) current += 1;
+    else {
+      maxSequence = Math.max(maxSequence, current);
+      current = 0;
+    }
+  }
+  return Math.max(maxSequence, current);
+};
+
+console.log(findMaxConsecutiveOnes([1,1,0,1,1,1]));
