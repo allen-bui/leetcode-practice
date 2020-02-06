84e31fccdb4745aee802898508edc171763db054
allen-bui <allen.vbui@gmail.com>
2020-02-06 14:51:41 -0800

    re-completes dynamic programming fibonacci sequence

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,18 @@
 // Time Complexity -  O(N)
 // Space Complexity - O(N)
 
-/**
- * @param {number} N
- * @return {number}
- */
-var fib = function(N) {
-  if (N === 0) return 0;
-  const arr = [];
-  arr[0] = 1;
-  arr[1] = 1;
+function fibonacci(n) {
 
-  for (let i = 2; i < N; ++i) {
-    arr[i] = arr[i - 1] + arr[i - 2];
+  if (n === 0) {
+    return 0;
   }
-  return arr[N - 1];
-};
+
+  const arr = [1, 1];
+
+  for (let i = 2; i < n; ++i) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+  }
+  return arr[n - 1];
+}
+
+console.log(fibonacci(6)); // 1, 1, 2, 3, 5, 8
