997e996bf42d55cae9606fcf5d80124e9d5ad448
allen-bui <allen.vbui@gmail.com>
2020-02-06 15:58:05 -0800

    completes longest substring with no repeat characters

--- longestSubstringNoRepeat.js
+++ longestSubstringNoRepeat.js
@@ -4,40 +4,21 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-  /*
-  Pseudocode
-  ----------
-  1. create a max and current variable
-  2. add edge case where s length is one
-  3. use two for loops, outer loop to loop through each letter
-  4. inner loop will add words into a hash set and check to see if it's been seen before
-  5. compare current to max, and take the greater length
-  */
-
-  if (s.length === 1) return 1;
-
-  let current = '';
-  let max = 0;
-  let seen = {};
+  const lastSeen = {};
+  let longest = [0, 1];
+  let startIndex = 0;
 
   for (let i = 0; i < s.length; ++i) {
-
-    seen[s[i]] = true;
-    current = s[i];
-
-    for (let j = 1; j < s.length; ++j) {
-
-      if (seen[s[j]] === undefined) {
-        seen[s[j]] = true;
-        current += s[j];
-        if (current.length === s.length) return current.length;
-        continue;
-      }
+    const currentChar = s[i];
+    if (currentChar in lastSeen) {
+      startIndex = Math.max(startIndex, lastSeen[currentChar] + 1);
     }
-    max = Math.max(current.length, max);
-    seen = {};
-    current = '';
-  }  return max;
+    if (longest[1] - longest[0] < i + 1 - startIndex) {
+      longest = [startIndex, i + 1];
+    }
+    lastSeen[currentChar] = i;
+  }
+  return s.slice(longest[0], longest[1]);
 };
 
 console.log(lengthOfLongestSubstring('pwwkew')); // 1
