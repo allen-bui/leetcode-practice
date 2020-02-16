f029314186099043d1a6346d509a20f8f354ad9e
allen-bui <allen.vbui@gmail.com>
2020-02-16 14:25:23 -0800

    completes word search

---
+++ wordSearch.js
@@ -0,0 +1,43 @@
+function wordSearch(board, word) {
+  for (let i = 0; i < board.length; ++i) {
+    for (let j = 0; j < board[i].length; ++j) {
+      if (board[i][j] === word[0] && dfs(board, i, j, 0, word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+
+  function dfs(board, i, j, count, word) {
+    if (count === word.length) {
+      return true;
+    }
+    if (
+      i < 0 ||
+      i >= board.length ||
+      j < 0 ||
+      j >= board[i].length ||
+      board[i][j] !== word[count]
+    ) {
+      return false;
+    }
+
+    const temp = board[i][j];
+    board[i][j] = " ";
+    const found =
+      dfs(board, i + 1, j, count + 1, word) ||
+      dfs(board, i - 1, j, count + 1, word) ||
+      dfs(board, i, j + 1, count + 1, word) ||
+      dfs(board, i, j - 1, count + 1, word);
+    board[i][j] = temp;
+    return found;
+  }
+}
+
+const board = [
+  ["A", "B", "C", "E"],
+  ["S", "F", "C", "S"],
+  ["A", "D", "E", "E"]
+];
+
+console.log(wordSearch(board, "ABCCED")); // true
