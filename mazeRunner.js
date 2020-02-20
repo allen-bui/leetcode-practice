e01793a2885b822f3634c333d45334e3dac85560
allen-bui <allen.vbui@gmail.com>
2020-02-19 18:22:09 -0800

    re-completes maze runner for practice with dfs

--- mazeRunner.js
+++ mazeRunner.js
@@ -5,52 +5,29 @@
  */
 
 function isTherePath(board) {
-
   if (dfs(board, 0, 0)) return true;
-  else return false;
+  return false;
+}
 
-  function dfs(board, row, col) {
-
-    // Checks bounds and our current position
-    if (
-      row < 0 ||
-      row >= board.length ||
-      col < 0 ||
-      col >= board[row].length ||
-      board[row][col] === 0
-    ) {
-      return false;
-    }
-
-    // Checks the end exist and makes sure row and col is at the end
-    if (
-      board[board.length - 1][board[row].length - 1] === 1 &&
-      row === board.length - 1 &&
-      col === board[row].length - 1
-    ) {
-      return true;
-    }
-
-    // Sinks current position to '0' to mark as visited
-    board[row][col] = 0;
-
-    // Continue path in all directions
-    const found =
-      dfs(board, row + 1, col) ||
-      dfs(board, row - 1, col) ||
-      dfs(board, row, col + 1) ||
-      dfs(board, row, col - 1);
-    board[row][col] === 1;
-    return found;
-  }
+// helper dfs function
+function dfs(board, row, col) {
+  if (row < 0 || col < 0 || row >= board.length || col >= board[row].length || board[row][col] === 0) return false;
+  if (row === board.length - 1 && col === board[row].length - 1) return true;
+  board[row][col] = 0;
+  const isPathFound = dfs(board, row + 1, col)
+    || dfs(board, row - 1, col)
+    || dfs(board, row, col + 1)
+    || dfs(board, row, col - 1)
+  board[row][col] = 1;
+  return isPathFound;
 }
 
 const board = [
   [1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 1],
+  [0, 1, 0, 0, 1],
   [1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0],
-  [1, 1, 1, 1, 1]
-];
+  [1, 0, 1, 0, 0],
+  [1, 1, 1, 1, 0],
+]
 
-console.log(isTherePath(board)); // true;
+console.log(isTherePath(board)); // true
