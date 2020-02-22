235b32cb79fb18827da02711c3233e637cd907c0
allen-bui <allen.vbui@gmail.com>
2020-02-21 16:39:07 -0800

    re-completes number of islands

--- numberOfIslands.js
+++ numberOfIslands.js
@@ -2,41 +2,45 @@
  * @Time  - O(M * N) where M is rows and N is columns
  * @Space - O(M * N)
  */
+
 function numberOfIslands(board) {
 
   let numberOfIslands = 0;
 
   for (let row = 0; row < board.length; ++row) {
     for (let col = 0; col < board[row].length; ++col) {
-      if (board[row][col] === 1) {
-        numberOfIslands += dfs(board, row, col);
+      if (board[row][col] === 1 && dfs(board, row, col)) {
+        numberOfIslands += 1;
       }
     }
   }
   return numberOfIslands;
+}
 
-  // helper function
-  function dfs(board, row, col) {
+function dfs(board, row, col) {
 
-    if (row < 0 || col < 0 || row >= board.length || col >= board[row].length || board[row][col] === 0) {
-      return 0;
-    }
-    board[row][col] = 0;
-    dfs(board, row + 1, col);
-    dfs(board, row - 1, col);
-    dfs(board, row, col + 1);
-    dfs(board, row, col - 1);
-    return 1;
-  }
+  if (
+    row < 0
+    || col < 0
+    || row >= board.length
+    || col >= board[row].length
+    || board[row][col] === 0
+  ) return false;
+
+  board[row][col] = 0;
+  dfs(board, row + 1, col);
+  dfs(board, row - 1, col);
+  dfs(board, row, col + 1);
+  dfs(board, row, col - 1);
+  return 1;
 }
 
 const board = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 1],
-  [1, 1, 0, 1, 1, 1],
-  [1, 1, 0, 1, 1, 1],
-  [1, 1, 0, 1, 1, 1],
-]
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0],
+  [0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1],
+];
 
 console.log(numberOfIslands(board)); // 3
