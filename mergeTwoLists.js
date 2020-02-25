de2634815e82d14c261a3c0689651bbc158af43b
allen-bui <allen.vbui@gmail.com>
2020-02-25 01:06:52 -0800

    re-completes merge two lists

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -8,33 +8,36 @@ class Node {
   }
 }
 
-function mergeLinkedList(listOne, listTwo) {
+function mergeSortedList(listOne, listTwo) {
 
-  let mergedList = new Node();
-  let current = mergedList;
+  // declare variables
+  let merge = new Node();
+  let current = merge;
 
   while (listOne && listTwo) {
     if (listOne.value < listTwo.value) {
-      mergedList.next = new Node(listOne.value);
+      merge.next = new Node(listOne.value);
       listOne = listOne.next;
     } else {
-      mergedList.next = new Node(listTwo.value);
+      merge.next = new Node(listTwo.value);
       listTwo = listTwo.next;
     }
-    mergedList = mergedList.next;
+    merge = merge.next;
   }
 
-  if (listOne) mergedList.next = listOne;
-  if (listTwo) mergedList.next = listTwo;
+  // check remaining list
+  if (listOne) merge.next = listOne;
+  if (listTwo) merge.next = listTwo;
 
+  // return result;
   return current.next;
 }
 
-const linkedListOne = new Node(1);
-linkedListOne.next = new Node(3);
-linkedListOne.next.next = new Node(5);
+const llOne = new Node(1);
+llOne.next = new Node(4);
 
-const linkedListTwo = new Node(2);
-linkedListTwo.next = new Node(4);
+const llTwo = new Node(2);
+llTwo.next = new Node(3);
+llTwo.next.next = new Node(5);
 
-console.log(mergeLinkedList(linkedListOne, linkedListTwo));
+console.log(mergeSortedList(llOne, llTwo)); // 1 -> 2 -> 3 -> 4 -> 5
