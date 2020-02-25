85cce31dfe035745e6b72d501c2deed26a5b9290
allen-bui <allen.vbui@gmail.com>
2020-02-25 01:13:37 -0800

    re-completes word search

--- wordSearch.js
+++ wordSearch.js
@@ -6,10 +6,9 @@
  */
 
 function wordSearch(board, word) {
-
   for (let row = 0; row < board.length; ++row) {
     for (let col = 0; col < board[row].length; ++col) {
-      if (word[0] === board[row][col] && dfs(board, row, col, 0, word)) {
+      if (word[0] === board[row][col] && dfs(row, col, 0, word, board)) {
         return true;
       }
     }
@@ -17,23 +16,21 @@ function wordSearch(board, word) {
   return false;
 }
 
-function dfs(board, row, col, count, word) {
-
+function dfs(row, col, count, word, board) {
   if (word.length === count) return true;
-  if (
-    row < 0
+  if (row < 0
     || col < 0
     || row >= board.length
     || col >= board[row].length
-    || board[row][col] !== word[count]
+    || word[count] !== board[row][col]
   ) return false;
 
   const temp = board[row][col];
-  board[row][col] = null;
-  const found = dfs(board, row + 1, col, count + 1, word)
-  || dfs(board, row - 1, col, count + 1, word)
-  || dfs(board, row, col + 1, count + 1, word)
-  || dfs(board, row, col - 1, count + 1, word);
+  board[row][col] = '';
+  const found = dfs(row + 1, col, count + 1, word, board)
+    || dfs(row - 1, col, count + 1, word, board)
+    || dfs(row, col + 1, count + 1, word, board)
+    || dfs(row, col - 1, count + 1, word, board);
   board[row][col] = temp;
   return found;
 }
@@ -41,9 +38,10 @@ function dfs(board, row, col, count, word) {
 const board = [
   ['A', 'A', 'H', 'A', 'A'],
   ['A', 'A', 'E', 'A', 'A'],
-  ['A', 'A', 'L', 'A', 'A'],
-  ['A', 'A', 'L', 'A', 'A'],
-  ['A', 'A', 'O', 'A', 'A'],
-];
+  ['A', 'A', 'L', 'L', 'A'],
+  ['A', 'A', 'A', 'O', 'A'],
+  ['A', 'A', 'A', 'A', 'A'],
+]
 
 console.log(wordSearch(board, 'HELLO')); // true
+
